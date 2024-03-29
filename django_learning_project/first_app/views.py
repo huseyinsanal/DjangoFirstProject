e369c9b2a623e4827075b0f3747c754ffeee8ea7
Hüseyin Şanal <huseyinnsanal@gmail.com>
2024-03-30 00:32:53 +0300

    updated

--- django_learning_project/first_app/views.py
+++ django_learning_project/first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseNotFound, HttpResponseRedirect
-
+from django.urls import reverse
 # Create your views here.
 
 course_dictionary = {
@@ -28,5 +28,10 @@ def multiply_view(request, num1, num2):
 
 def course_number_view(request, num1):  #huseyinsanal.com/10 -> huseyinsanal.com/first_app/kotlin
     course_list = list(course_dictionary.keys())
-    course = course_list[num1]
-    return HttpResponseRedirect(f"/first_app/{course}")
+    try:
+            
+        course = course_list[num1]
+        page_to_go = reverse("course",args=[course])
+        return HttpResponseRedirect(page_to_go)
+    except:
+        HttpResponseNotFound("Not Found! Please look for another course!")
